6274b3c52156d1ae8a6b5846b90d74422b4ef8bc
Hugo Thiessard <hugo.thiessard@pm.me>
2019-07-06 03:03:16 +0200

    Too many changes

--- main.py
+++ main.py
@@ -5,15 +5,36 @@ from datetime import datetime
 from time import sleep
 import config
 
+
 my_comments = []
 
-def invest(submission, investment):
+
+def pre_invest(submission, investment):
     for comment in submission.comments:
         if comment.author == "MemeInvestor_Bot":
-            my_reply = comment.reply("!invest " + str(investment))
+            #submission.downvote()
+            #my_reply = comment.reply("!invest " + str(investment))
             print("https://reddit.com" + my_reply.permalink)
-            my_comments.append((my_reply, submission))
+            #my_comments.append((my_reply, submission))
             break
+
+
+def post_invest(comment, submission):
+    comment.refresh()
+    for reply in comment.replies:
+        if reply.author == "MemeInvestor_Bot":
+            print("Reply of " + submission.title)
+            submission.upvote()
+            break
+
+
+def invest_in_sub(submissions, score, age):
+    for submission in submissions:
+        sub_age = (datetime.now() - datetime.fromtimestamp(submission.created_utc)).total_seconds() / (60 * 60);
+        if submission.subreddit == "MemeEconomy" and sub_age <= age and submission.score >= score:
+            print(submission.title, "https://reddit.com" + submission.permalink, submission.score, sub_age)
+            pre_invest(submission, config.investment)
+
 
 def main():
     reddit = praw.Reddit(**config.reddit)
@@ -21,26 +42,16 @@ def main():
     sub_all = reddit.subreddit('All')
     sub_meme = reddit.subreddit('MemeEconomy')
 
-    for submission in sub_all.hot(limit=1000):
-        if submission.subreddit == "MemeEconomy":
-            hour = (datetime.now() - datetime.fromtimestamp(submission.created_utc)).total_seconds() / (60 * 60);
-            print(submission.title, "https://reddit.com" + submission.permalink, submission.score, hour)
-            submission.downvote()
-            invest(submission, config.investment)
-
+    invest_in_sub(sub_all.hot(limit=1000), 100, 3)
+    invest_in_sub(sub_meme.new(limit=1000), 30, 0.3)
     print("End of prospecting")
 
     while len(my_comments) != 0:
         sleep(2 * 60)
         print("I have " + str(len(my_comments)) + " comments")
         for comment, submission in my_comments[:]:
-            comment.refresh()
-            for reply in comment.replies:
-                if reply.author == "MemeInvestor_Bot":
-                    print("Reply of " + submission.title)
-                    submission.upvote()
-                    my_comments.remove((comment, submission))
-                    break
+            post_invests(comment, submission)
+            my_comments.remove((comment, submission))
 
 
 if __name__ == "__main__":
