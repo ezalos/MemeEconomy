96e62cd0ed6766bfd3a02264461692ce8ac0696f
Hugo Thiessard <hugo.thiessard@pm.me>
2019-07-06 06:03:44 +0200

    Add Investment class

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import praw
 from datetime import datetime
 from time import sleep
 import config
-
-
-my_investments = []
-
+from investment import Investment, State
 
 def pre_invest(submission, investment):
     for comment in comment.submission.comments:
@@ -18,7 +15,6 @@ def pre_invest(submission, investment):
             my_investments.append(my_reply)
             break
 
-
 def post_invest(comment):
     comment.refresh()
     for reply in comment.replies:
@@ -27,13 +23,13 @@ def post_invest(comment):
             comment.submission.upvote()
             break
 
-
-def invest_in_sub(submissions, score = 0, age = 10):
+def find_worth(submissions, score = 0, age = 10):
+    investments = []
     for submission in submissions:
         sub_age = (datetime.now() - datetime.fromtimestamp(submission.created_utc)).total_seconds() / (60 * 60);
         if submission.subreddit == "MemeEconomy" and sub_age <= age and submission.score >= score:
-            print(submission.title, "https://reddit.com" + submission.permalink, submission.score, sub_age)
-            pre_invest(submission, config.investment)
+            investments.append(Investment(submission))
+    return investments
 
 def background_check():
     while len(my_investments) != 0:
@@ -43,16 +39,23 @@ def background_check():
             post_invests(comment)
             my_investments.remove(comment)
 
+def find_investments(reddit):
+    sub_all = reddit.subreddit('All')
+    sub_meme = reddit.subreddit('MemeEconomy')
+    investments = []
+    investments += is_worth(sub_all.hot(limit=1000), 100, 3)
+    investments += is_worth(sub_meme.new(limit=1000), 30, 0.3)
+    return investments
+
 def main():
     reddit = praw.Reddit(**config.reddit)
 
-    sub_all = reddit.subreddit('All')
-    sub_meme = reddit.subreddit('MemeEconomy')
+    investments = find_investments(reddit)
 
-    invest_in_sub(sub_all.hot(limit=1000), 100, 3)
-    invest_in_sub(sub_meme.new(limit=1000), 30, 0.3)
+    # invest_in_sub(sub_all.hot(limit=1000), 100, 3)
+    # invest_in_sub(sub_meme.new(limit=1000), 30, 0.3)
     print("End of prospecting")
-    background_check()
+    background_check(investments)
 
 
 if __name__ == "__main__":
